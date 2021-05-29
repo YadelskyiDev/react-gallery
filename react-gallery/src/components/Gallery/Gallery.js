9a0d8d9276ab2d3c9ec84b1b6d6cd64d60179390
Viktor <yadelskyiviktor@gmail.com>
2021-05-29 21:36:34 +0300

    slider warning fix

--- react-gallery/src/components/Gallery/Gallery.js
+++ react-gallery/src/components/Gallery/Gallery.js
@@ -2,7 +2,7 @@ import { Slider } from '../Slider/Slider'
 
 
 const images = [
-    'https://images.musement.com/cover/0002/13/san-francisco-masterlu-fotolia-com-m-jpg_header-112309.jpeg',
+    'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l0eXxlbnwwfHwwfHw%3D&w=1000&q=80',
     'https://cdn1.matadornetwork.com/blogs/1/2011/10/Kiev-Ukraine-cityscape-destinations-1200x899.jpg',
     'https://www.washingtonpost.com/resizer/Q3gSZn4URJz_lw1lkyPaUhzjPkY=/arc-anglerfish-washpost-prod-washpost/public/DBLLHPDG5AI6TJUYFKHYBDE47M.jpg'
     
--- react-gallery/src/components/Slider/Slider.js
+++ react-gallery/src/components/Slider/Slider.js
@@ -19,9 +19,9 @@ export const Slider = props => {
 
     const swipeRef = useRef(null);
 
-  
+    const { autoPlay, slides } = props;
     const { translate, slideTranslate, transition, activeIndex, startX, clickDown, deltaX, autoPlayAvailable } = state;
-    const autoPlayRef = useRef()
+    const autoPlayRef = useRef();
 
     useEffect(() => {
         autoPlayRef.current = nextSlide;
@@ -29,26 +29,26 @@ export const Slider = props => {
 
     useEffect(() => {
         const play = () => {
-            autoPlayRef.current()
+            autoPlayRef.current();
         }
-        if(props.autoPlay !== null){
+        if(autoPlay !== null){
             const interval = setInterval(() => {
                 if(autoPlayAvailable) {
-                    play()
+                    play();
                 }
-            }, props.autoPlay * 1000)
+            }, autoPlay * 1000)
             return () => clearInterval(interval)
         }
-    }, [props.autoPlay, autoPlayAvailable])
+    }, [autoPlay, autoPlayAvailable])
 
     const nextSlide = useCallback(stateReset => {
-        if(activeIndex === props.slides.length - 1) {
+        if(activeIndex === slides.length - 1) {
             return setState({
                 ...state,
                 ...stateReset,
                 translate: 0,
                 slideTranslate: 0,
-                activeIndex: 0
+                activeIndex: 0,
             })
         }
         const translate = (activeIndex + 1) * getWidth()
@@ -59,19 +59,19 @@ export const Slider = props => {
             translate,
             slideTranslate: translate
         })
-    },[activeIndex, props.slides.length, state])
+    },[activeIndex, slides.length, state])
 
 
     const prevSlide = useCallback(stateReset => {
         if(activeIndex === 0){
-            const translate = (props.slides.length - 1) * getWidth()
+            const translate = (slides.length - 1) * getWidth()
 
             return setState({
                 ...state,
                 ...stateReset,
                 translate,
                 slideTranslate: translate,
-                activeIndex: props.slides.length - 1,
+                activeIndex: slides.length - 1
             })
         }
 
@@ -80,47 +80,48 @@ export const Slider = props => {
             ...state,
             ...stateReset,
             activeIndex: activeIndex - 1,
-            translate,
+            translate: translate,
             slideTranslate: translate
         })
-    },[activeIndex, props.slides.length, state])
+    },[activeIndex, slides.length, state])
  
 
 
     const mouseDownHandler = useCallback(e => {
         return setState({
             ...state,
-            startX: e.clientX,
+            startX: e.screenX,
             clickDown: true,
             autoPlayAvailable: false,
         })
     },[state])
      
     const mouseMoveHandler = useCallback (e => {
-        if(clickDown === true){
-            const delta = e.clientX - startX
-            setState({
+        if(clickDown){
+            const delta = e.screenX - startX
+            return setState({
                 ...state,
                 deltaX: delta,
-                slideTranslate: translate - delta
+                slideTranslate: translate - delta,
             }) 
         }
     }, [state, translate, clickDown, startX])
     
 
-    const mouseUpHandler = useCallback(e => {
+    const mouseUpHandler = useCallback(() => {
         const halfWidth = getWidth() / 2
         const stateReset = {
-            startX: null,
-            deltaX: 0,
             clickDown: false,
+            startX: 0,
+            deltaX: 0,
             autoPlayAvailable: true,
             slideTranslate: translate
         }
-        if(deltaX > halfWidth){
+        
+        if(deltaX >= halfWidth){
             prevSlide(stateReset)
         }
-        else if(deltaX < -halfWidth){
+        else if(deltaX <= -halfWidth){
             nextSlide(stateReset)
         } else {
             return setState({
@@ -133,40 +134,45 @@ export const Slider = props => {
 
     useEffect(()=>{
         const el = swipeRef.current;
-        el.removeEventListener('mousemove', mouseMoveHandler)
-        el.removeEventListener('mousedown', mouseDownHandler)
         el.removeEventListener('mouseup', mouseUpHandler)
+        el.removeEventListener('mousedown', mouseDownHandler)
 
         el.addEventListener('mousedown', mouseDownHandler)
         
-        if(clickDown === true){
-            el.addEventListener('mousemove', mouseMoveHandler)
-            el.addEventListener('mouseup', mouseUpHandler)
-        }
+        document.addEventListener('mousemove', mouseMoveHandler)
+        document.addEventListener('mouseup', mouseUpHandler)
+        
         return () => {
-            el.removeEventListener('mousemove', mouseMoveHandler)
+            document.removeEventListener('mousemove', mouseMoveHandler)
             el.removeEventListener('mousedown', mouseDownHandler)
-            el.removeEventListener('mouseup', mouseUpHandler)
+            document.removeEventListener('mouseup', mouseUpHandler)
+          
         }
     },[swipeRef, mouseDownHandler,  mouseUpHandler,mouseMoveHandler, clickDown])
 
-    // const styles = {
-    //     transform: `translateX(-${slideTranslate}px)`
-    // transition: transform ease-out .45s;
-    // }
+    const styles = {
+        transform: `translateX(-${slideTranslate}px)`,
+        height: `100%`,
+        width: `${getWidth() * slides.length}px`,
+        display:`flex`,
+        transition: `transform ease-out ${transition}s`
+    }
+        
+
 
     return(
         <SliderWrapper>
-            <SliderContent
+            {/* <SliderContent
                 translate={slideTranslate}
                 transition={transition}
-                width={getWidth() * props.slides.length}>
-                <div  ref={swipeRef}>
+                width={getWidth() * slides.length}
+                > */}
+                <div  ref={swipeRef} style={styles}>
                     {
-                        props.slides.map((slide, i) => <Slide key={slide + i} content={slide}/>)
+                        slides.map((slide, i) => <Slide key={slide + i} content={slide}/>)
                     }
                 </div>
-            </SliderContent>
+            {/* </SliderContent> */}
             <Arrow direction='right' handleClick={nextSlide}/>
             <Arrow direction='left' handleClick={prevSlide} />
         </SliderWrapper>
